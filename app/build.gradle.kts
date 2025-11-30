e6b4b32c384daf54e708a9407bff9bce8bdc2824
qscrim <tochnonemaltsev@yandex.ru>
2025-11-30 19:45:21 +0700

    Изменение в версиях и зависимостях

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,8 @@ android {
 
 dependencies {
 
+    implementation( libs.androidx.glance.appwidget )
+    implementation( libs.androidx.glance.material3)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
